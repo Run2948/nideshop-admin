7210a4b57aec51a729e08bd3f6364da468acd2e9
zhujinrun <15997352948@163.com>
2020-11-25 17:21:23 +0800

    Init Project

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'nideshop-admin'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
